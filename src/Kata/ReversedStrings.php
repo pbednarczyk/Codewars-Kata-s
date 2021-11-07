75b959d662dd701bbc6027e7e999b5fc47b3a82f
pbednarczyk <ibednar88@gmail.com>
2021-11-07 22:19:28 +0100

    Reversed Words

---
+++ src/Kata/ReversedStrings.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Kata;
+
+/**
+ * @author Paweł Bednarczyk <ibednar88@gmail.com>
+ */
+class ReversedStrings
+{
+    public function solution($str): string {
+        return strrev($str);
+    }
+}
